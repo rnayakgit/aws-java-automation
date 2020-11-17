0e24d6050ceacf414fbf7e18543ced8c4796b550
Ranjan Kumar Nayak <ranjan.nayak@EXIINF-IM-386.local>
2020-11-17 09:58:37 +0530

    initial commit

---
+++ src/main/java/com/aws/java/application/AwsJavaAutomationApplication.java
@@ -0,0 +1,13 @@
+package com.aws.java.application;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AwsJavaAutomationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AwsJavaAutomationApplication.class, args);
+	}
+
+}
